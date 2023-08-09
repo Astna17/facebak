94f7520350f38ab690f5cd229bf63cfe9143f249
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2023-08-09 10:30:51 +0300

    doc : "second reorganisastion docs"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
-import { Main } from './main/body.jsx';
+import { Header } from './main/header.jsx';
+import { NavBar } from './main/nav-bar';
+import { Post } from './modules/post/post';
+import "./main/main.css";
+// import { Profil } from './modules/profil/profil.jsx';
+// import { Login } from './modules/login-singin/login';
 
 function App() {
   return (
-    <div className="App">
-      <Main/>
-    </div>
+    <>
+      <Header />
+        <main>
+          <NavBar />
+          <Post />
+          {/* <Profil/> */}
+          {/* <Login/>  */}
+        </main>
+    </>
   );
 }
 
