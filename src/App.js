3a2b9339deda87e4ee6fa66cf561a9d055eadad8
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2023-08-12 09:01:09 +0300

    feat : " chore : teplate parce postList"

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Header } from './main/header.jsx';
 import { NavBar } from './main/nav-bar';
 // import { Post } from './modules/post/post';
 import "./main/main.css";
+// import { PostList } from './modules/post/1-post-list';
 import { NewPost } from './modules/post/new-post';
 // import { Profil } from './modules/profil/profil.jsx';
 // import { Login } from './modules/login-singin/login';
@@ -13,6 +14,7 @@ function App() {
       <Header />
         <main>
           <NavBar />
+          {/* <PostList/> */}
           <NewPost/>
           {/* <Post /> */}
           {/* <Profil/> */}
